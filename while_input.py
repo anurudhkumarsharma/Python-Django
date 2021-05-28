b3671d50642fa1ec5da37c732ba8ff8b75218843
anurudh kumar sharma <anurudhkumarsharma014@gmail.com>
2021-05-28 19:26:52 +0530

    added file

---
+++ while_input.py
@@ -0,0 +1,11 @@
+def func():
+    print('\n'*10)
+    result=input("           Hello Rajat Sharma!\nHow are you?\n")
+    if result=='good':
+       print("That's Cool")
+    else:
+       while result!='good':
+         result=input("I will keep asking this ,until you are not good.If you want me to stop, then type 'good':")
+    print(result)
+func()
+    
