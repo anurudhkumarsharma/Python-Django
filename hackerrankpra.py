bc5ca1effa17e93418be0e5168d9bb30afd0154d
anurudh kumar sharma <anurudhkumarsharma014@gmail.com>
2021-05-28 18:06:18 +0530

    added file

---
+++ hackerrankpra.py
@@ -0,0 +1,14 @@
+print('\n'*100)
+def func(n):
+    if n%2==0:
+        if n in range(2,6):
+            print('Not Weired')
+        elif n in range(6,21):
+            print('Not Weired')
+        else:
+            print('Not Weired')
+    elif n%2!=0:
+        print('Weired')   
+    else:
+        pass
+func(9)
