0229cfc0a17168822272adf9696a63464ecc5bc0
anurudh kumar sharma <anurudhkumarsharma014@gmail.com>
2022-12-29 13:19:02 +0530

    added file

--- django/Store247/mac/mac/urls.py
+++ django/Store247/mac/mac/urls.py
@@ -1,7 +1,6 @@
-"""textutils URL Configuration
-
+"""mac URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,18 +13,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
-    path('analyze',views.analyze,name='analyze'),
-    #path('contact',views.contact,name='contact'),
-    #path('feature',views.feature,name='feature'),
-    #path('capitalizefirst',views.capfirst,name='capitalizefirst'),
-    #path('newlineremove',views.newlineremove,name='capfirst'),
-    #path('spaceremove',views.spaceremove,name='space remove'),
-    #path('charcount',views.charcount,name='charcount'),
-    
-
-]
+    path('shop/', include('shop.urls')),
+    path('blog/', include('blog.urls')),
+    path('', views.index)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/textutils/textutils/views.py
+++ django/textutils/textutils/views.py
@@ -1,84 +1,50 @@
-# I have created this file
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
-
 def index(request):
-    return render(request, 'index.html')
-def contact(request):
-    return render(request, 'contact.html')
-
-def feature(request):
-    return render(request, 'feature.html')
+    return render(request,"index.html")
 
 def analyze(request):
-    # get the text
+    # Get the text
     djtext = request.POST.get('text', 'default')
 
-    # Check checkbox values
-    removepunc = request.POST.get('removepunc', 'off')
-    fullcaps = request.POST.get('fullcaps', 'off')
-    newlineremover = request.POST.get('newlineremover', 'off')
-    extraspaceremover = request.POST.get('extraspaceremover', 'off')
-    charcount = request.POST.get('charcount', 'off')
+    removepunc=request.POST.get('removepunc','off')
+    fullcaps=request.POST.get('fullcaps','off')
+    newlineremover=request.POST.get('newlineremover','off')
+    extraspaceremover=request.POST.get('extraspaceremover','off')
 
-    # check with checkbox is on
     if removepunc == "on":
-        punctuations = '''!()-{,}[];:'"\,<>./?@#$%^&*_~'''
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
-        params = {'purpose': 'Removed puncuations', 'analyzed_text': analyzed}
-        djtext = analyzed
-
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext=analyzed
         # return render(request, 'analyze.html', params)
-
-    if(fullcaps == "on"):
-        analyzed = ""
+    if fullcaps=="on":
+        analyzed=""
         for char in djtext:
-            analyzed = analyzed + char.upper()
-        params = {'purpose': 'Changed to Upper', 'analyzed_text': analyzed}
-    # analyze the text
-        djtext = analyzed
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext=analyzed
         # return render(request, 'analyze.html', params)
-
-    if(extraspaceremover == 'on'):
+    if newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+    if (extraspaceremover == "on"):
         analyzed = ""
         for index, char in enumerate(djtext):
-            if not(djtext[index] == " " and djtext[index+1] == " "):
+            if not (djtext[index] == " " and djtext[index + 1] == " "):
                 analyzed = analyzed + char
-        params = {'purpose': 'Removed Extra Space', 'analyzed_text': analyzed}
-    # analyze the text
-        djtext = analyzed
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext=analyzed
         # return render(request, 'analyze.html', params)
-
-    if(newlineremover == 'on'):
-        analyzed = ""
-        for char in djtext:
-            if char != '\n' and char != '\r':
-                analyzed = analyzed + char
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-    # analyze the text
-        djtext = analyzed
-        # return render(request, 'analyze.html', params)
-
-    if(charcount == 'on'):
-        count = 0
-        for i in djtext:
-            count += 1
-        params = {'purpose': 'Number of CHaracters', 'analyzed_text': count}
-    if(removepunc != "on" and fullcaps != "on" and extraspaceremover != "on" and newlineremover != "on" and charcount != "on"):
-        return HttpResponse("Please select atleast one box!")
 
     return render(request, 'analyze.html', params)
-
- # def capfirst(request):
-#    return HttpResponse("This is a Capitalize first")
-# def newlineremove(request):
-#    return HttpResponse("This is a New line remove")
-# def spaceremove(request):
-#    return HttpResponse("This is a Space remove")
-# def charcount(request):
-#   return HttpResponse("This is a Char count")
