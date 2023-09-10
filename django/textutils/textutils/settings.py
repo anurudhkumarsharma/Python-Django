a7ee9b847304dffadd5e3e1f894861b11bebfda9
anurudh kumar sharma <anurudhkumarsharma014@gmail.com>
2023-09-10 16:04:15 +0530

    added file

--- django/textutils/textutils/settings.py
+++ django/textutils/textutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y4l8g1v4q#3$rsqkzmw$6h4e%sfxi_sa$2kzwotg@h@ygn(ig0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -117,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
